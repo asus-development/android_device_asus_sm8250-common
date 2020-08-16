3a36a57a2ab44259eb1ebe7f5e5fa4ce848fcc8e
micky387 <mickaelsaibi@free.fr>
2020-08-16 05:31:12 +0200

    sm8250-common: Enable Airtrigger
    Change-Id: I055d61361c71948410f3480172e9b3899f1fde75

--- DeviceParts/src/org/omnirom/device/DeviceSettings.java
+++ DeviceParts/src/org/omnirom/device/DeviceSettings.java
@@ -19,17 +19,20 @@ package org.omnirom.device;
 
 import android.app.AlertDialog;
 import android.app.Dialog;
+import android.content.Context;
 import android.content.pm.ApplicationInfo;
 import android.content.pm.PackageManager;
 import android.content.res.Resources;
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.UserHandle;
 import androidx.preference.PreferenceFragment;
 import androidx.preference.ListPreference;
 import androidx.preference.Preference;
 import androidx.preference.PreferenceCategory;
 import androidx.preference.PreferenceScreen;
 import androidx.preference.TwoStatePreference;
+import androidx.preference.SwitchPreference;
 import android.provider.Settings;
 import android.text.TextUtils;
 import android.view.MenuItem;
@@ -45,11 +48,25 @@ public class DeviceSettings extends PreferenceFragment implements
     public static final String KEY_SETTINGS_PREFIX = "device_setting_";
     public static final String KEY_GLOVE_SWITCH = "glove";
 
+    private static final String KEY_CATEGORY_GAMING = "category_gaming";
     private static final String KEY_CATEGORY_SCREEN = "screen";
     public static final String KEY_GAME_GENIE = "game_toolbar_app";
 
+    private static final boolean sIsRog3 = android.os.Build.MODEL.equals("ASUS_I003D");
+    private static final String ACTION_AIR_TRIGGER_OFF = "com.asus.airtriggers.SYSTEMUI_AIR_TRIGGER_OFF";
+    private static final String ACTION_AIR_TRIGGER_ON = "com.asus.airtriggers.SYSTEMUI_AIR_TRIGGER_ON";
+    private static final String AIRTRIGGER_PACKAGE_NAME = "com.asus.airtriggers";
+    private static final String FIELD_AIR_TRIGGER_ENABLE = "air_trigger_enable";
+    public static final String KEY_AIRTRIGGER = "grip_sensor_apk";
+    public static final String KEY_AIRTRIGGER_PREF = "grip_sensor_pref";
+    private static final String TAG = "AirTriggerApkPreferenceController";
+
+    private Airtrigger mAirtrigger;
     private static TwoStatePreference mGloveModeSwitch;
+    private static Preference mAirtriggerPref;
+    private static Preference mGameCategory;
     private static Preference mGameGenie;
+    private static SwitchPreference mGripSensorPreference;
 
     @Override
     public void onCreatePreferences(Bundle savedInstanceState, String rootKey) {
@@ -60,6 +77,17 @@ public class DeviceSettings extends PreferenceFragment implements
         mGloveModeSwitch.setChecked(GloveModeSwitch.isCurrentlyEnabled(this.getContext()));
         mGloveModeSwitch.setOnPreferenceChangeListener(new GloveModeSwitch(getContext()));
 
+        mAirtriggerPref = findPreference(KEY_AIRTRIGGER_PREF);
+        mGameCategory = findPreference(KEY_CATEGORY_GAMING);
+        
+        mGripSensorPreference = (SwitchPreference) findPreference(KEY_AIRTRIGGER);
+        mGripSensorPreference.setChecked(Settings.Global.getInt(getContext().getContentResolver(),
+        FIELD_AIR_TRIGGER_ENABLE, 0) == 1);
+        mGripSensorPreference.setOnPreferenceChangeListener(this);
+        if (!sIsRog3) {
+            getPreferenceScreen().removePreference(mGameCategory);
+        }
+
         mGameGenie = findPreference(KEY_GAME_GENIE);
         mGameGenie.setEnabled(GameGenie.isGameGenieExist(this.getContext()));
 
@@ -67,11 +95,25 @@ public class DeviceSettings extends PreferenceFragment implements
 
     @Override
     public boolean onPreferenceTreeClick(Preference preference) {
+        if (preference == mAirtriggerPref) {
+            mAirtrigger.startAirTriggerSettings(this.getContext());
+        }
         return super.onPreferenceTreeClick(preference);
     }
 
     @Override
     public boolean onPreferenceChange(Preference preference, Object newValue) {
+        if (preference == mGripSensorPreference) {
+            notifySwitchState(((Boolean) newValue).booleanValue());
+        }
         return true;
+    }
+
+    private void notifySwitchState(boolean z) {
+        Log.d(TAG, "notifySwitchState enabled=" + z);
+        Intent intent = new Intent();
+        intent.setAction(z ? ACTION_AIR_TRIGGER_ON : ACTION_AIR_TRIGGER_OFF);
+        intent.setPackage(AIRTRIGGER_PACKAGE_NAME);
+        getContext().sendBroadcastAsUser(intent, UserHandle.CURRENT);
     }
 }
--- DeviceParts/src/org/omnirom/device/Startup.java
+++ DeviceParts/src/org/omnirom/device/Startup.java
@@ -26,6 +26,8 @@ import android.provider.Settings;
 import android.text.TextUtils;
 
 public class Startup extends BroadcastReceiver {
+    private static final String ASUS_GAMEMODE = "asus_gamemode";
+
     private static void restore(String file, boolean enabled) {
         if (file == null) {
             return;
@@ -59,6 +61,7 @@ public class Startup extends BroadcastReceiver {
     public void onReceive(final Context context, final Intent bootintent) {
         maybeImportOldSettings(context);
         restoreAfterUserSwitch(context);
+        context.startService(new Intent(context, GripSensorServiceMain.class));
     }
 
     public static void restoreAfterUserSwitch(Context context) {
@@ -122,6 +125,26 @@ public class Startup extends BroadcastReceiver {
         }
         enabled = !value.equals(AppSelectListPreference.DISABLED_ENTRY);
         restore(GestureSettings.getGestureFile(GestureSettings.KEY_Z_APP), enabled);
+
+        value = Settings.Global.getString(context.getContentResolver(), ASUS_GAMEMODE);
+        if (TextUtils.isEmpty(value)) {
+            value = "0";
+            if (Utils.isCNSKU()) {
+                Settings.System.putString(context.getContentResolver(), "asus_grip_short_squeeze", "6");
+                Settings.System.putString(context.getContentResolver(), "asus_grip_locked_short_squeeze", "6");
+            } else {
+                Settings.System.putString(context.getContentResolver(), "asus_grip_short_squeeze", "4");
+                Settings.System.putString(context.getContentResolver(), "asus_grip_locked_short_squeeze", "4");
+            }
+            Settings.System.putString(context.getContentResolver(), "asus_grip_long_squeeze", "6");
+            Settings.System.putString(context.getContentResolver(), "asus_grip_locked_long_squeeze", "6");
+            Settings.Global.putString(context.getContentResolver(), ASUS_GAMEMODE, value);
+
+            Settings.Global.putString(context.getContentResolver(), "air_trigger_squeeze_threshold_level", "5");
+            Settings.Global.putString(context.getContentResolver(), "air_trigger_tap_left_threshold_level", "4");
+            Settings.Global.putString(context.getContentResolver(), "air_trigger_tap_right_threshold_level", "4");
+            Settings.Global.putString(context.getContentResolver(), "air_trigger_tap_threshold_level", "4");
+        }
 
         enabled = Settings.System.getInt(context.getContentResolver(), GloveModeSwitch.SETTINGS_KEY, 0) != 0;
         if (enabled) {
